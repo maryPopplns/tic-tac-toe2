a07f01d08728fde6e9ef68a2b23de685d725dbe2
Spencer Knight <spencer.knightusn@gmail.com>
2021-07-12 20:23:11 -0700

    saving before test

--- src/components/game_board/board/board.js
+++ src/components/game_board/board/board.js
@@ -10,7 +10,7 @@ const BOARD = () => {
   const GRID_SEVEN = document.createElement("div");
   const GRID_EIGHT = document.createElement("div");
   const GRID_NINE = document.createElement("div");
-  const GRIDS = [
+  const TILE_CONTAINERS = [
     GRID_ONE,
     GRID_TWO,
     GRID_THREE,
@@ -22,15 +22,35 @@ const BOARD = () => {
     GRID_NINE,
   ];
 
+  const IDS = [
+    "grid_one",
+    "grid_two",
+    "grid_three",
+    "grid_four",
+    "grid_five",
+    "grid_six",
+    "grid_seven",
+    "grid_eight",
+    "grid_nine",
+  ];
+
   BOARD_CONTAINER.setAttribute("id", "board_container");
   for (let i = 0; i < 9; i++) {
-    GRIDS[i].setAttribute("id", `tile_${i}`);
-    GRIDS[i].setAttribute("class", `ttt_tile`);
+    TILE_CONTAINERS[i].setAttribute("id", `tile_container_${i}`);
+    TILE_CONTAINERS[i].setAttribute("class", `tile_container`);
   }
   for (let i = 0; i < 9; i++) {
-    BOARD_CONTAINER.append(GRIDS[i]);
+    BOARD_CONTAINER.append(TILE_CONTAINERS[i]);
   }
   GAME_CONTAINER.append(BOARD_CONTAINER);
+
+  TILE_CONTAINERS.map((tile_container, id) => {
+    for (let i = 0; i < 64; i++) {
+      const TILE = document.createElement("div");
+      TILE.setAttribute("class", `board_tile ${IDS[id]}`);
+      tile_container.append(TILE);
+    }
+  });
 };
 
 export { BOARD };
---
+++ src/game_logic/game_logic.js
@@ -0,0 +1,5 @@
+let game_board = {
+  moves = [],
+};
+
+export { game_board };
